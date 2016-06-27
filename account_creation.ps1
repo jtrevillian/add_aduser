b3a5a3997e121239f5507dd28e26ec1ae2070d62
bboyd1987 <brendenrboyd@gmail.com>
2016-06-27 13:06:20 -0400

    Changed EVERYTHING (#5)
    Updated some variable names.  Added logic to close windows when switching between them.  Added EDI Check and Error Window.

--- account_creation.ps1
+++ account_creation.ps1
@@ -1,15 +1,41 @@
-﻿# This form will create an account based upon the information provided in the fields.
+#Creates the initial EDI Check Form.
+
+
+# This form will create an account based upon the information provided in the fields.
 # After clicking "Create" it will prompt for a password in the original powershell window     (should still be open in the background.
 # Once the account is created it will be set to "Smart Card Required" using the upn field followed by "@company"
 # This can be changed by changing "SmartCardLogonRequired" to $false.  Note if you leave it enabled and then uncheck it manually you will have to reset their password
 
 # Set up the form         =========================================================    ==
 
-Function MakeNewForm {
+Function NewCheckEDIForm {
 
 	$objForm.Close()
 	$objForm.Dispose()
-	MakeForm
+	CheckEDIForm
+
+}
+
+Function CloseErrorForm {
+
+    $errorForm.Close()
+	$errorForm.Dispose()
+	CheckEDIForm
+
+}
+
+Function CloseEDIFormEDIExists {
+
+    $CheckEDIForm.Close()
+    $CheckEDIForm.Dispose()
+    EDIExists
+
+}
+Function CloseEDIFormMakeForm {
+    
+    $CheckEDIForm.Close()
+    $CheckEDIForm.Dispose()
+    MakeForm
 
 }
 
@@ -22,7 +48,7 @@ Function NewUsersHDrive {
     #$UPN = '1234567890@mil'
     #$HomeDrive=’Z:’
 
-$UserRoot= '\\ServerName\users\'
+$UserRoot= '\\Server\users\'
 $HomeDirectory=$UserRoot+$script:legacy
 
 # SET-ADUSER $AccountName –HomeDrive $HomeDrive –HomeDirectory $HomeDirectory 
@@ -30,7 +56,7 @@ $HomeDirectory=$UserRoot+$script:legacy
 # Create the folder on the root of the common Users Share
 
 NEW-ITEM –path $HomeDirectory -type directory 
-$Domain=’Domain’
+$Domain=’DOMAIN’
 $IdentityReference=$Domain+’\’+$script:legacy
 
 # Set parameters for Access rule
@@ -51,8 +77,6 @@ $HomeFolderACL = GET-ACL $HomeDirectory
 $HomeFolderACL.AddAccessRule($AccessRule)
 SET-ACL –path $HomeDirectory -AclObject $HomeFolderACL
 }
-
-
 
 Function MakeForm {
 
@@ -80,7 +104,7 @@ $objForm.KeyPreview = $True
 $FirstNameLabel = New-Object System.Windows.Forms.Label
 
 $FIRSTNAME = New-Object System.Windows.Forms.TextBox
-$UnlockAccountButton = New-Object     System.Windows.Forms.Button
+$UnlockAccountButton = New-Object System.Windows.Forms.Button
 $InitialState = New-Object System.Windows.Forms.FormWindowState 
 
 #placing fields ===============================
@@ -181,10 +205,11 @@ $UPNLabel.Size = New-Object System.Drawing.Size (100,15)
 $UPNLabel.Text = "EDIPI"
 $objForm.Controls.Add($UPNLabel)
 
-$objUPNTextBox = New-Object System.Windows.Forms.TextBox 
-$objUPNTextBox.Location = New-Object System.Drawing.Size(290,85) 
-$objUPNTextBox.Size = New-Object System.Drawing.Size(250,20) 
-$objForm.Controls.Add($objUPNTextBox)
+$objUPNText = New-Object System.Windows.Forms.Label 
+$objUPNText.Location = New-Object System.Drawing.Size(290,85) 
+$objUPNText.Size = New-Object System.Drawing.Size(250,20) 
+$objUPNText.Text = "$script:UPN"
+$objForm.Controls.Add($objUPNText)
 
 $TitleLabel = New-Object System.Windows.Forms.Label
 $TitleLabel.Location = New-Object System.Drawing.Size (290,110)
@@ -209,7 +234,7 @@ $objExpire.Size = New-Object System.Drawing.Size(250,20)
 $objForm.Controls.Add($objExpire) 
 # Button and action         ========================================================
 
-$CreateAccountButton = New-Object     System.Windows.Forms.Button
+$CreateAccountButton = New-Object System.Windows.Forms.Button
 
 $CreateAccountButton.Size = New-Object System.Drawing.Size (100,30)
 
@@ -225,7 +250,6 @@ $CreateAccountButton.Add_Click(
 $userfirst=$objFirstNameTextBox.Text
 $userlast=$objTextBox.Text
 $usermiddle = $objMiddleTextBox.Text
-$UPN = $objUPNTextBox.Text
 $Title = $objTitle.Text
 $script:legacy = $objLegacy.Text
 $email = $objEmailTextBox.Text
@@ -233,16 +257,16 @@ $phoneNumber = $objPhoneTextBox.Text
 $office = $objOfficeTextBox.Text
 $name = $userlast +", " + $userfirst + " " + $usermiddle
 $displayname = $userlast +", " + $userfirst + " " + $usermiddle + " " + $Title
-$principalName = $UPN + "@mil"
+$principalName = $script:UPN
 $expireDateDefault = $objExpire.SelectionStart
 $expireDate = $expireDateDefault.ToShortDateString()
 $securepw = $objPasswordTextBox.Text | ConvertTo-SecureString -AsPlainText -Force
 
-New-ADUser -SamAccountName $script:legacy -GivenName $userfirst -Surname $userlast -Initials $usermiddle -Name $name -DisplayName $displayname -Description $displayname -Path 'PATH' -SmartcardLogonRequired $true -AccountPassword $securepw -UserPrincipalName  $principalName -Enabled $true -EmailAddress $email -AccountExpirationDate $expireDate -HomeDrive "H:" -HomeDirectory "\\3usaa7swan100\users\$legacy" -PasswordNeverExpires $true -Office $office -OfficePhone $phoneNumber
+New-ADUser -SamAccountName $script:legacy -GivenName $userfirst -Surname $userlast -Initials $usermiddle -Name $name -DisplayName $displayname -Description $displayname -Path 'WE NEED DA OU' -SmartcardLogonRequired $true -AccountPassword $securepw -UserPrincipalName  $principalName -Enabled $true -EmailAddress $email -AccountExpirationDate $expireDate -HomeDrive "H:" -HomeDirectory "\\3usaa7swan100\users\$legacy" -PasswordNeverExpires $true -Office $office -OfficePhone $phoneNumber
 
 NewUsersHDrive
 
-MakeNewForm
+NewCheckEDIForm
 
 }
 )
@@ -256,4 +280,96 @@ $objForm.Add_Shown({$objForm.Activate()})
 [void] $objForm.ShowDialog()
 }
 
-MakeForm
+Function CheckEDIForm {
+
+[void]     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void]     [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+
+import-module ActiveDirectory
+
+$CheckEDIForm = New-Object System.Windows.Forms.Form
+$CheckEDIForm.Text = "Check if user exists:"
+$CheckEDIForm.Size = New-Object System.Drawing.Size(300,175)
+$CheckEDIForm.StartPosition = "CenterScreen"
+$CheckEDIForm.KeyPreview = $True
+
+$CheckEDILabel = New-Object System.Windows.Forms.Label
+$CheckEDILabel.Location = New-Object System.Drawing.Size (25,25)
+$CheckEDILabel.Size = New-Object System.Drawing.Size (125,15)
+$CheckEDILabel.Text = "Enter the User's EDIPI:"
+$CheckEDIForm.Controls.Add($CheckEDILabel)
+
+$CheckEDITextBox = New-Object System.Windows.Forms.TextBox 
+$CheckEDITextBox.Location = New-Object System.Drawing.Size(27,45) 
+$CheckEDITextBox.Size = New-Object System.Drawing.Size(130,20) 
+$CheckEDIForm.Controls.Add($CheckEDITextBox)
+
+$CheckEDIButton = New-Object System.Windows.Forms.Button
+$CheckEDIButton.Size = New-Object System.Drawing.Size (75,30)
+$CheckEDIButton.Location = New-Object System.Drawing.Size (100,85)
+$CheckEDIButton.Text = "Check EDI"
+$CheckEDIForm.Controls.Add($CheckEDIButton)
+
+$CheckEDIButton.Add_Click(
+{
+
+$UPN = $CheckEDITextBox.Text
+$script:UPN = $UPN + '@mil'
+$UPNCheck = get-aduser -filter "userprincipalname -like '$script:UPN'" | select -ExpandProperty userprincipalname
+
+if ($UPNCheck -eq $null)
+    {
+    CloseEDIFormMakeForm
+    }
+
+else 
+    {
+    CloseEDIFormEDIExists
+    }
+}
+)
+
+$CheckEDIForm.Add_Shown({$CheckEDIForm.Activate()})
+[void] $CheckEDIForm.ShowDialog()
+
+}
+
+Function EDIexists
+{
+$distinguishedName = get-aduser -Filter "userprincipalname -like '$script:upn'" | select -ExpandProperty distinguishedname
+
+$errorForm = New-Object System.Windows.Forms.Form
+$errorForm.Text = "User already exists"
+$errorForm.Size = New-Object System.Drawing.Size(500,175)
+$errorForm.StartPosition = "CenterScreen"
+$errorForm.KeyPreview = $True
+
+$UPNLabel = New-Object System.Windows.Forms.Label
+$UPNLabel.Location = New-Object System.Drawing.Size (25,25)
+$UPNLabel.Size = New-Object System.Drawing.Size (175,15)
+$UPNLabel.Text = "EDPI: $script:UPN"
+$errorForm.Controls.Add($UPNLabel)
+
+$distinguishedNameLabel = New-Object System.Windows.Forms.Label 
+$distinguishedNameLabel.Location = New-Object System.Drawing.Size(27,45) 
+$distinguishedNameLabel.Size = New-Object System.Drawing.Size(400,50)
+$distinguishedNameLabel.Text = "Distinguished Name: $distinguishedName" 
+$errorForm.Controls.Add($distinguishedNameLabel)
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Size = New-Object System.Drawing.Size (75,30)
+$OKButton.Location = New-Object System.Drawing.Size (100,100)
+$OKButton.Text =  'OK'
+$errorForm.Controls.Add($OKButton)
+
+$OKButton.Add_Click(
+    {
+    CloseErrorForm
+    }
+)
+
+$errorForm.Add_Shown({$errorForm.Activate()})
+[void] $errorForm.ShowDialog()
+}
+
+CheckEDIForm
